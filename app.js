60f15da257371e4168a59100d75086c13e9ac121
Adain <adain@edsoft.com>
2021-01-02 15:23:46 +0530

    firstCommit

---
+++ app.js
@@ -0,0 +1,15 @@
+// jshint esversion:6
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const request = require("request");
+
+app.use(express.static(__dirname));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+  });
+
+app.listen(3000, function() {
+    console.log("Server running at port 3000");
+  });
